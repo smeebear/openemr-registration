50ed7b1b0e56f5051d6fd67e5b313e2e19cf96fc
Sam O'Heron <soheron@gmail.com>
2024-11-12 14:46:59 -0600

    new script to create OEMR client

---
+++ createClient.sh
@@ -0,0 +1,11 @@
+curl -X POST -k -H 'Content-Type: application/json' -i https://localhost/oauth2/default/registration --data '{
+   "application_type": "private",
+   "redirect_uris":
+     ["http://localhost:3000/callback"],
+   "post_logout_redirect_uris":
+     ["http://localhost:3000/logout/callback"],
+   "client_name": "OpenEMR Patient Self Registration",
+   "token_endpoint_auth_method": "client_secret_post",
+   "contacts": ["me@example.org", "them@example.org"],
+   "scope": "openid offline_access api:oemr api:fhir api:port user/allergy.read user/allergy.write user/appointment.read user/appointment.write user/dental_issue.read user/dental_issue.write user/document.read user/document.write user/drug.read user/encounter.read user/encounter.write user/facility.read user/facility.write user/immunization.read user/insurance.read user/insurance.write user/insurance_company.read user/insurance_company.write user/insurance_type.read user/list.read user/medical_problem.read user/medical_problem.write user/medication.read user/medication.write user/message.write user/patient.read user/patient.write user/practitioner.read user/practitioner.write user/prescription.read user/procedure.read user/soap_note.read user/soap_note.write user/surgery.read user/surgery.write user/transaction.read user/transaction.write user/vital.read user/vital.write user/AllergyIntolerance.read user/CareTeam.read user/Condition.read user/Coverage.read user/Encounter.read user/Immunization.read user/Location.read user/Medication.read user/MedicationRequest.read user/Observation.read user/Organization.read user/Organization.write user/Patient.read user/Patient.write user/Practitioner.read user/Practitioner.write user/PractitionerRole.read user/Procedure.read patient/AllergyIntolerance.read patient/CareTeam.read patient/Condition.read patient/Coverage.read patient/Encounter.read patient/Immunization.read patient/MedicationRequest.read patient/Observation.read patient/Patient.read patient/Procedure.read"
+  }' > secrets.json
