e3a0365228e6f9edc5d1422102adafcb6923f70e
Sam O'Heron <soheron@gmail.com>
2024-11-12 10:35:10 -0600

    no more conditional rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 // import Image from "next/image";
 "use client";
-import { useState } from "react";
+// import { useState } from "react";
 
 import { formSubmission } from "./actions";
 export default function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  // const [loggedIn, setLoggedIn] = useState(false);
 
-  if (loggedIn) {
-    return <div>Logged In</div>;
-  }
+  // if (loggedIn) {
+  //   return <div>Logged In</div>;
+  // }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
